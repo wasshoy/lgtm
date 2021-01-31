457a7f314b6c49dc67819d60bb2822793064ff72
A.Arimura <404ari@gmail.com>
2021-01-31 16:24:59 +0900

    テスト修正

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 import unittest
 
+
 class LgtmTest(unittest.TestCase):
     def test_lgtm(self):
         from lgtm.core import lgtm
-        self.assertIsNone(lgtm())
+        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))
